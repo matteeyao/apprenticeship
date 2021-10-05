1c9e6adce8d5eb90688a12e95543635e191e9610
matteeyao <myaoski@gmail.com>
2021-10-05 08:36:09 -0500

    Add Board class

---
+++ App/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            /* At most pass newly instaniated classes to new classes, no processing messages
+             Tests: Does it instantiate all of the following classes that it needs*/
+        }
+    }
+}
